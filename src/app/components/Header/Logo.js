342db60b81ef6c56301f5fc7dee2b75443de392a
Brian Burns <brian.david.burns@gmail.com>
2024-07-16 12:18:06 -0500

    Added logo for dark mode, unsucessfully tried to implement logo toggle.

--- src/app/components/Header/Logo.js
+++ src/app/components/Header/Logo.js
@@ -1,20 +1,41 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import logoImg from '@/public/BLPC-logo-short-bw.png'
+import darkLogoImg from '@/public/BLPC-logo-short-ws.png' // Import the dark mode logo image
 
 const Logo = () => {
+  const [mode, setMode] = useState("light"); // Set the initial mode state
+
+  // Use useEffect to set up initial mode and listen for theme changes
+  useEffect(() => {
+    const userPreference = window.localStorage.getItem("theme");
+    if (userPreference) {
+      setMode(userPreference);
+    } else {
+      const preferDarkQuery = "(prefers-color-scheme: dark)";
+      setMode(window.matchMedia(preferDarkQuery).matches ? "dark" : "light");
+    }
+
+    const handleThemeChange = () => {
+      const newMode = window.localStorage.getItem("theme");
+      if (newMode) {
+        setMode(newMode);
+      }
+    };
+
+    window.addEventListener("storage", handleThemeChange);
+
+    return () => {
+      window.removeEventListener("storage", handleThemeChange);
+    };
+  }, []);
+
   return (
     <Link href="/" className="flex items-center text-dark">
-      {/* <div className="w-12 h-12 rounded-full overflow-clip flex justify-center items-center"> */}
       <div className="w-30 h-12 flex items-center justify-center">
-  <Image src={logoImg} alt="blog logo" className="object-contain h-full w-full" />
-</div>
-
-
-
-
-      {/* <span className="text-accent font-bold text-xl dark:text-light">Brian Burns</span> */}
+        <Image src={mode === "dark" ? darkLogoImg : logoImg} alt="blog logo" className="object-contain h-full w-full" />
+      </div>
     </Link>
   )
 }

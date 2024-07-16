e59cad4508af18bf14bab960ffed89097ef803e3
Brian Burns <brian.david.burns@gmail.com>
2024-07-16 06:04:57 -0500

    Updated metadata and favicon

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,21 +1,21 @@
 // please update required information
 const siteMetadata = {
-    title: 'Brian Burns - Tech, Politics, and All Things Chicago',
+    title: 'Burns Law P.C.',
     author: 'Brian Burns',
-    headerTitle: 'Next.js Blog',
-    description: 'Site Created with Next.js, Tailwind.css, and contentlayer, based off of tutorial by CodeBucks.',
+    headerTitle: 'Burns Law P.C.',
+    description: 'Family law, divorce law, and child support attorney serving Chicago, Cook County, Lake County, and DuPage County.',
     language: 'en-us',
     theme: 'light', // system, dark or light
-    siteUrl: 'https://www.brianburns.com', // your website URL
+    siteUrl: 'https://www.burnslawpc.com', // your website URL
     siteLogo: '/logo.png',
-    socialBanner: '/social-banner.png', // add social banner in the public folder
-    email: 'brian@brianburns.com', 
-    github: 'https://github.com/bbgits',
-    twitter: 'https://twitter.com/burnsUSA',
-    facebook: 'https://facebook.com',
-    youtube: 'https://youtube.com',
+    // socialBanner: '/social-banner.png', // add social banner in the public folder
+    email: 'brian@burnslawpc.com', 
+    // github: 'https://github.com/bbgits',
+    // twitter: 'https://twitter.com/burnsUSA',
+    // facebook: 'https://facebook.com',
+    // youtube: 'https://youtube.com',
     linkedin: 'https://www.linkedin.com/in/brian-burns-b9107921/',
-    dribbble: 'https://www.dribbble.com',
+    // dribbble: 'https://www.dribbble.com',
     locale: 'en-US',
   }
   

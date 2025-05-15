f006aafb7db14471e5a700eaf97b830d652ccd4f
bbgits <brian.david.burns@gmail.com>
2025-05-15 10:03:17 -0500

    Updated blog post to have image in the body

--- burns-law-web/src/app/(about)/about/page.js
+++ burns-law-web/src/app/(about)/about/page.js
@@ -1,89 +1,85 @@
 // ~/src/app/(about)/about/page.js
-
 "use client";
+
 import AboutCoverSection from "../../components/About/AboutCoverSection";
 import Skills from "../../components/About/Skills";
-import React, { useState } from "react";
-import { collection, addDoc } from "firebase/firestore"; // Import the necessary Firestore methods
-import { db } from "@/firebaseConfig";
-// import FormMain from "../../components/About/FormMain";
-import { getDatabase } from "firebase/database";
-import firebase from "firebase/compat/app";
+import React, { useState, useEffect } from "react";
+import { collection, addDoc } from "firebase/firestore"; // Import Firestore methods
+import { db } from "../../../../firebaseConfig"; // Import Firestore configuration
 import FormMain from "../../components/About/FormMain";
-import FormEasy from "../../components/About/FormEasy";
-import FormSimple from "../../components/About/FormSimple"
-
-// export const metadata = {
-//   title: "About Burns Law P.C.",
-//   description: "Here are some details about my self.",
-// };
 
 export default function About() {
+  const [isClient, setIsClient] = useState(false);
+
+  // Ensure this component runs only on the client side
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <>
       <AboutCoverSection />
-      {/* <FormSimple />
-      <FormEasy /> */}
       <FormMain />
       <Skills />
-      <h2 className="font-bold text-3xl xs:text-4xl sxl:text-5xl  text-center lg:text-left mt-4 text-dark dark:text-light">
-        affordable pricing options
+      <h2 className="font-bold text-3xl xs:text-4xl sxl:text-5xl text-center lg:text-left mt-4 text-dark dark:text-light">
+        Affordable Pricing Options
       </h2>
 
       <h2 className="mt-8 font-semibold text-lg md:text-2xl self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal">
-        <span className="font-extrabold underline">
-          {" "}
-          1 Hr Consult ($225)
-        </span>{" "}
-       Video Call to discuss your matter and help you understand your legal options. Payment due at start of meeting, Book Here. {" "}
+        <span className="font-extrabold underline">1 Hr Consult ($225)</span>{" "}
+        Video Call to discuss your matter and help you understand your legal
+        options. Payment due at start of meeting, Book Here.{" "}
       </h2>
-      <button
-        onClick={() => {
-          window.open('https://calendly.com/brian-burnslawpc/legal-consult', '_blank');
-        }}
-        className="bg-green-900 hover:bg-green-950 text-md text-white font-bold mt-4 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-      >
-        Book 1 Hr Consult ($225 Due at Mtg.)
-      </button>
+
+      {/* Ensure the button with window.open only renders on the client */}
+      {isClient && (
+        <button
+          onClick={() => {
+            window.open(
+              "https://calendly.com/brian-burnslawpc/legal-consult",
+              "_blank"
+            );
+          }}
+          className="bg-green-900 hover:bg-green-950 text-md text-white font-bold mt-4 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Book 1 Hr Consult ($225 Due at Mtg.)
+        </button>
+      )}
 
       <h2 className="mt-8 font-semibold text-lg md:text-2xl self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal">
-        <span className="font-extrabold underline">
-          {" "}
-          Pro-Se PLUS (from $300)
-        </span>{" "}
+        <span className="font-extrabold underline">Pro-Se PLUS (from $300)</span>{" "}
         In law, 'Pro Se' means that a person is representing themselves in court
         (I will not be representing you). This limited scope engagement includes
-        a 1 hr initial consult where I will explain court proceedures and
-        provide guidance on getting started. I can also help you draft several
-        of the most common pleadings and final agreements so that you are ready
-        to represent yourself effectively.{" "}
+        a 1 hr initial consult where I will explain court procedures and provide
+        guidance on getting started. I can also help you draft several of the
+        most common pleadings and final agreements so that you are ready to
+        represent yourself effectively.{" "}
       </h2>
 
       <h2 className="mt-8 font-semibold text-lg md:text-2xl self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal">
         <span className="font-extrabold underline">
-          {" "}
           Flat Fee(s) Divorce (from $2,500)
         </span>{" "}
         For straightforward cases, I offer a Flat Fee option that includes
         filing standardized pleadings and representing you in court. While I
-        cannot gaurantee that the Flat Fee will be sufficient to complete your
-        case, we provide clear contingencies and pre-priced ('add-on') services so
-        that you know how much certain legal detours could cost ahead of time.
+        cannot guarantee that the Flat Fee will be sufficient to complete your
+        case, we provide clear contingencies and pre-priced ('add-on') services
+        so that you know how much certain legal detours could cost ahead of
+        time.
       </h2>
 
       <h2 className="mt-8 font-semibold text-lg md:text-2xl self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal">
         <span className="font-extrabold underline">
-          {" "}
-          Classic Retainer(from $3,500)
+          Classic Retainer (from $3,500)
         </span>{" "}
         For most cases, a standard retainer agreement is the best option. By
-        paying your initial retainer, you have pre-paid for (&#34retained&#34) my time
+        paying your initial retainer, you have pre-paid for ("retained") my time
         to work on your matter. This means that I will be able to move quicker
         to act on your behalf and prioritize your case.
       </h2>
 
-      <h2 className="font-bold text-xl xs:text-2xl sxl:text-3xl  text-center lg:text-left mt-12 text-dark dark:text-light">
-        want to learn more?
+      <h2 className="font-bold text-xl xs:text-2xl sxl:text-3xl text-center lg:text-left mt-12 text-dark dark:text-light">
+        Want to Learn More?
       </h2>
 
       <h2 className="mt-2 font-semibold text-lg md:text-2xl self-center text-center mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal">
@@ -92,3 +88,90 @@ export default function About() {
     </>
   );
 }
+
+// "use client";
+// import AboutCoverSection from "../../components/About/AboutCoverSection";
+// import Skills from "../../components/About/Skills";
+// import React, { useState } from "react";
+// import FormMain from "../../components/About/FormMain";
+
+
+// // export const metadata = {
+// //   title: "About Burns Law P.C.",
+// //   description: "Here are some details about my self.",
+// // };
+
+// export default function About() {
+//   return (
+//     <>
+//       <AboutCoverSection />
+//       {/* <FormSimple />
+//       <FormEasy /> */}
+//       <FormMain />
+//       <Skills />
+//       <h2 className="font-bold text-3xl xs:text-4xl sxl:text-5xl  text-center lg:text-left mt-4 text-dark dark:text-light">
+//         affordable pricing options
+//       </h2>
+
+//       <h2 className="mt-8 font-semibold text-lg md:text-2xl self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal">
+//         <span className="font-extrabold underline">
+//           {" "}
+//           1 Hr Consult ($225)
+//         </span>{" "}
+//        Video Call to discuss your matter and help you understand your legal options. Payment due at start of meeting, Book Here. {" "}
+//       </h2>
+//       <button
+//         onClick={() => {
+//           window.open('https://calendly.com/brian-burnslawpc/legal-consult', '_blank');
+//         }}
+//         className="bg-green-900 hover:bg-green-950 text-md text-white font-bold mt-4 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+//       >
+//         Book 1 Hr Consult ($225 Due at Mtg.)
+//       </button>
+
+//       <h2 className="mt-8 font-semibold text-lg md:text-2xl self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal">
+//         <span className="font-extrabold underline">
+//           {" "}
+//           Pro-Se PLUS (from $300)
+//         </span>{" "}
+//         In law, 'Pro Se' means that a person is representing themselves in court
+//         (I will not be representing you). This limited scope engagement includes
+//         a 1 hr initial consult where I will explain court proceedures and
+//         provide guidance on getting started. I can also help you draft several
+//         of the most common pleadings and final agreements so that you are ready
+//         to represent yourself effectively.{" "}
+//       </h2>
+
+//       <h2 className="mt-8 font-semibold text-lg md:text-2xl self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal">
+//         <span className="font-extrabold underline">
+//           {" "}
+//           Flat Fee(s) Divorce (from $2,500)
+//         </span>{" "}
+//         For straightforward cases, I offer a Flat Fee option that includes
+//         filing standardized pleadings and representing you in court. While I
+//         cannot gaurantee that the Flat Fee will be sufficient to complete your
+//         case, we provide clear contingencies and pre-priced ('add-on') services so
+//         that you know how much certain legal detours could cost ahead of time.
+//       </h2>
+
+//       <h2 className="mt-8 font-semibold text-lg md:text-2xl self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal">
+//         <span className="font-extrabold underline">
+//           {" "}
+//           Classic Retainer(from $3,500)
+//         </span>{" "}
+//         For most cases, a standard retainer agreement is the best option. By
+//         paying your initial retainer, you have pre-paid for (&#34retained&#34) my time
+//         to work on your matter. This means that I will be able to move quicker
+//         to act on your behalf and prioritize your case.
+//       </h2>
+
+//       <h2 className="font-bold text-xl xs:text-2xl sxl:text-3xl  text-center lg:text-left mt-12 text-dark dark:text-light">
+//         want to learn more?
+//       </h2>
+
+//       <h2 className="mt-2 font-semibold text-lg md:text-2xl self-center text-center mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal">
+//         Just send me an email: brian@burnslawpc.com
+//       </h2>
+//     </>
+//   );
+// }
